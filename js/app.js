831819ebec704939293c3fc9856a4ede2b9935e3
violinapopova <violipopova@gmail.com>
2019-01-25 06:18:04 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,194 +1,138 @@
-// Start game
-// Initialize the game
-function startGame() {
-  // Check the selected character
-  let selectedCharacter = document.querySelector('.selected').id;
-
-  // Change the sprite of the character accordingly to the selected one
-  switch (selectedCharacter) {
-    case 'healer':
-      player.sprite = 'images/spaceship_healer_120X120.png';
-    break;
-    case 'dps':
-      player.sprite = 'images/spaceship_rogue_120X120.png';
-    break;
-    case 'tank':
-      player.sprite = 'images/spaceship_Tank_120X120.png';
-    break;
-  }
-}
-
-// Win Game
-// Make the win popup and overlay appear
-function winGame() {
-  const OVERLAY = document.getElementById('overlay');
-  OVERLAY.classList.remove('hidden');
-
-  const WIN = document.getElementById('win');
-  WIN.classList.remove('hidden');
-}
-
-// remove all selected classes
-// Useful to un-style the characters on the character's choice popup
-function removeSelectedClass() {
-  document.getElementById('healer').classList.remove('selected');
-  document.getElementById('dps').classList.remove('selected');
-  document.getElementById('tank').classList.remove('selected');
-}
-
-// Reset when colliding an enemy
-function reset() {
-  // Position of the player reset to 240, 600
-  player.x = 240;
-  player.y = 600;
-}
-
-// Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/Drone1_120X120.png';
-    // Take the position from the parameter
+let GameObjects = function (x, y) {
+    this.sprite;
+    this.x = 0;
+    this.y = 0;
+  };
+  
+  let Enemy = function(x, y, enemySpeed) {
+    this.enemySpeed = enemySpeed;
+    this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-    // Take the speed from the parameter
-    this.speed = speed;
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x += (this.speed * dt);
-    if (this.x >= 1024) this.x = -240;
-
-    // Check if the enemy is at the same location as the player
-    // If true, reset the position of the player
-    if (this.y === player.y) {
-      if (player.x >= this.x - 50 && player.x <= this.x + 50) reset();
+  
+  };
+  
+  /*Inheritted from GameObjects*/
+  Enemy.prototype = Object.create(GameObjects.prototype);
+  Enemy.prototype.constructor = Enemy;
+  
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  
+  Enemy.prototype.update = function (dt) {
+    this.x += this.enemySpeed * dt;
+    if (this.x >= 500) {
+      this.x=0;
     }
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(x, y) {
-  // Define the initial sprite to be used
-  this.sprite = 'images/spaceship_rogue_120X120.png';
-  // Take the position from the parameter
-  this.x = x;
-  this.y = y;
-};
-
-// Check if the player has won
-Player.prototype.update = function(dt) {
-  // If yes, make the winning popup appears
-  if (player.y === 0) winGame();
-};
-
-// Draw the player on the screen
-Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Manage the player's movement by using the arrow keys
-Player.prototype.handleInput = function(keycode) {
-  // Move the player accordingly to the key pressed
-  switch (keycode) {
-    case 'up':
-      if (player.y - 120 >= 0) player.y -= 120;
-    break;
-    case 'down':
-      if (player.y + 120 <= 600) player.y += 120;
-    break;
-    case 'left':
-      if (player.x - 120 >= 0) player.x -= 120;
-    break;
-    case 'right':
-      if (player.x + 120 <= 480) player.x += 120;
-    break;
-  }
-};
-
-// Now instantiate your objects.
-// Place the player object in a variable called player
-let player = new Player(240, 600);
-
-// Instantiate the enemies
-let enemy1 = new Enemy(0, 120, 250);
-let enemy2 = new Enemy(-360, 120, 326);
-let enemy3 = new Enemy(-120, 120, 542);
-let enemy4 = new Enemy(-240, 240, 704);
-let enemy5 = new Enemy(-360, 240, 298);
-let enemy6 = new Enemy(0, 240, 402);
-let enemy7 = new Enemy(-480, 360, 476);
-let enemy8 = new Enemy(-240, 360, 665);
-
-// Change the sprite of the enemies
-enemy1.sprite = 'images/Drone2_120X120.png';
-enemy4.sprite = 'images/Drone2_120X120.png';
-enemy6.sprite = 'images/Drone3_120X120.png';
-enemy2.sprite = 'images/Drone3_120X120.png';
-enemy3.sprite = 'images/Drone4_120X120.png';
-enemy7.sprite = 'images/Drone4_120X120.png';
-// Place all enemy objects in an array called allEnemies
-let allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6, enemy7, enemy8];
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+    areaOfEnemies = {x: this.x, y: this.y};
+    areaOfPlayer = {x: player.x, y: player.y};
+    if ((areaOfEnemies.x < (areaOfPlayer.x + 59)) && ((areaOfEnemies.x + 74) > areaOfPlayer.x) && (areaOfEnemies.y < (areaOfPlayer.y + 64)) && ((79 + areaOfEnemies.y) > areaOfPlayer.y)) {
+      setTimeout(player.reset(), 10000);
     };
-
-    player.handleInput(allowedKeys[e.keyCode]);
-});
-
-// Restart the game after winning if click on the restart button
-const RESTART = document.getElementById('restart');
-
-RESTART.addEventListener('click', function() {
-  location.reload();
-});
-
-// Choose the character to play
-const CHARACTER_SELECTION = document.getElementById('character-selection');
-
-CHARACTER_SELECTION.addEventListener('click', function(event) {
-  let characterChoosen = event.target;
-  // Check the id of the element or the parent
-  // When a character is clicked, remove the selected class for every character and add it to the selected one
-  if (event.target.id === 'healer' || event.target.parentElement.id === 'healer') {
-    removeSelectedClass();
-    document.getElementById('healer').classList.add('selected');
-  } else if (event.target.id === 'dps' || event.target.parentElement.id === 'dps') {
-    removeSelectedClass();
-    document.getElementById('dps').classList.add('selected');
-  } else if (event.target.id === 'tank' || event.target.parentElement.id === 'tank') {
-    removeSelectedClass();
-    document.getElementById('tank').classList.add('selected');
+  };
+  
+  // Draw the enemy on the screen, required method for game
+  Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
+  
+  // instantantiate objects
+  let bugA = new Enemy(1, 60, 120);
+  let bugB = new Enemy(1, 140, 160);
+  let bugC = new Enemy(1, 220, 90);
+  
+  // Enemies our player must avoid
+  const allEnemies = [bugA, bugB, bugC];
+  
+  let Character = function () {
+    GameObjects.call(this, Character);
+    this.sprite = 'images/char-horn-girl.png';
+    this.x = 200;
+    this.y = 380;
+  };
+  
+  // inherits from GameObjects
+  Character.prototype = Object.create(GameObjects);
+  Character.prototype.constructor = Character;
+  
+  Character.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
+  
+  // necessary for the win-window
+  let gamePanel = document.querySelector('.panel');
+  
+  // Change the player's position based on the user keyboard input
+  Character.prototype.handleInput = function(direction) {
+      if (direction == 'up') {
+          this.y = this.y - 80;
+      } else if (direction == 'down') {
+          this.y = this.y + 80;
+      } else if (direction == 'left') {
+          this.x = this.x - 101;
+      } else if (direction == 'right') {
+          this.x = this.x + 101;
+      } if (this.x < 0) {
+          this.x = 0;
+      } else if (this.x > 404) {
+          this.x = 404;
+    } else if (this.y > 404) {
+      // reset Character to starting position if he moves down outside the canvas
+      this.reset();
+    } else if (this.y == -20) {
+      winGame();
+    }
   }
-});
-
-// Start the game after player selection
-const START = document.getElementById('start');
-
-START.addEventListener('click', function() {
-  // Hide the popup
-  document.getElementById('character').classList.add('hidden');
-  document.getElementById('overlay').classList.add('hidden');
-  startGame();
-});
+  
+  // Reset the Character
+  Character.prototype.reset = function() {
+      this.x = 200;
+      this.y = 380;
+  }
+  
+  
+let player = new Character();
+  
+  // This listens for key presses and sends the keys to your
+  // Player.handleInput() method. You don't need to modify this.
+  document.addEventListener('keyup', function(e) {
+      var allowedKeys = {
+          37: 'left',
+          38: 'up',
+          39: 'right',
+          40: 'down'
+      };
+  
+      player.handleInput(allowedKeys[e.keyCode]);
+  });
+  
+  //winGame
+  
+  function winGame() {
+    // create html
+    const divHtml = document.createElement('div');
+    const h1Html = document.createElement('h1');
+  
+    // give it a class
+    divHtml.setAttribute('class', 'finished');
+    const inside = document.createElement('div');
+    inside.setAttribute('class', 'finished-inner');
+    divHtml.appendChild(inside);
+  
+    // insert it into the html
+    gamePanel.insertAdjacentElement('afterbegin', divHtml);
+    inside.appendChild(h1Html);
+  
+    // add text
+    h1Html.innerText =
+    `Congratulations!
+    You Successfully Finished the Game!`;
+  
+    // add play again button
+    const playAgain = document.createElement('a');
+    const playAgainText = document.createTextNode("Play again");
+    playAgain.appendChild(playAgainText);
+    playAgain.title = "Play again";
+    playAgain.href = "index.html";
+    inside.appendChild(playAgain);
+  }
